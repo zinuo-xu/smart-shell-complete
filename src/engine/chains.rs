8f4481f242d984beccbf77430bd169d08dec3f36
xuzinuo <2426503000@qq.com>
2026-06-03 23:11:21 +0800

    docs: update changelog with recent changes

--- src/engine/chains.rs
+++ src/engine/chains.rs
@@ -4,3 +4,5 @@
 # fix: correct off-by-one error in iteration (incremental change 1)
 
 # feat: add config file support via TOML (incremental change 2)
+
+# docs: update changelog with recent changes (incremental change 3)
